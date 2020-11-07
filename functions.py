1ddc10b80ad2cd19c7b91f8fcadccbcdb4df8525
Ayengar <mercelalex2@gmail.com>
2020-11-07 05:10:44 +0300

    2nd and 3d tasks in functions.py, working on 4th

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ def even_numbers():
                 if numbers[i] != ' ' :
                     single_number = single_number + numbers[i]
                 else:
-                    if single_number != '':
+                    if single_number.isdigit():
                         if int(single_number) % 2 == 0:
                             even_number_condition = 1
                     single_number = ''
@@ -18,19 +18,32 @@ def even_numbers():
                 print('There are even number(s)')
             else:
                 print('There are only odd numbers')
+
             
 # используй тернарный оператор, чтобы вызвать функцию
 # если возраст больше 21 года, в противном случае верни
 # сообщение "Мы не продаём алкоголь несовершеннолетним."
-# age = 17
-# sell_alcohol()
+#def sell_alcohol():
+#    print('Продаем алкоголь')
+#age = 17
+#sell_alcohol() if age >= 21 else print('Мы не продаём алкоголь несовершеннолетним.')
 
 
 # напиши функцию, которая проверит, является ли строка ключевым словом в Питоне
 # тебе понадобится модуль keyword, импортируй его и изучи с помощью dir()
-
+#def function_keyword():
+#    import keyword
+#    string = input()
+#    print('String is indeed a keyword in Python') if keyword.iskeyword(string) else print('String is not a keyword')
+#import keyword
+#print(keyword.kwlist)
+#function_keyword()
 
 # напиши функцию, которая возвращает тип данных на русском языке:
 # число, строка, булевый, None, список, кортеж, множество, словарь
 # пример: get_type("что-то") возвращает "Это строка."
 # пример2: get_type(42) возвращает "Это словарь."
+#checked_object = int(input())
+#def get_type():
+#    print(type(checked_object))
+#get_type()
