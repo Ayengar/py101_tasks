ccf6d2b1504c61d623527ef24ad0096ae771c4f4
Ayengar <72708903+Ayengar@users.noreply.github.com>
2020-11-08 23:37:48 +0300

    oof

--- word_freq.py
+++ word_freq.py
@@ -10,19 +10,23 @@
 import collections
 import argparse
 import os
+from pathlib import Path
 import nltk
+import string
 from nltk.corpus import stopwords
 
 parser = argparse.ArgumentParser(description='Top-100 words')
-parser.add_argument('dir', type=str, help='Input text directory')
+parser.add_argument('path', type=Path, help='Input text directory')
 args = parser.parse_args()
 path = args.path
 document = open(path, "r")
 text = document.read()
 
 stopwords = set(nltk.corpus.stopwords.words('english'))
+punctuation = string.punctuation
 text_manipulation = text.split()
 text_manipulation2 = [word for word in text_manipulation if word.lower() not in stopwords]
+text_manipulation2 = [word for word in text_manipulation2 if word.lower() not in punctuation]
 clean_text = ' '.join(text_manipulation2)
 
 words = []
