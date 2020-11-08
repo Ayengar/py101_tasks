f7940a9b0b41ca5baf5325a5723537451377c86f
Ayengar <72708903+Ayengar@users.noreply.github.com>
2020-11-08 22:12:06 +0300

    Update word_freq.py
    need correction

--- word_freq.py
+++ word_freq.py
@@ -7,12 +7,26 @@
 
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
-import os
+import collections
 import argparse
-import nltk
+import os
+#import nltk
 from nltk.corpus import stopwords
+
 parser = argparse.ArgumentParser(description='Top-100 words')
 parser.add_argument('dir', type=str, help='Input text directory')
 args = parser.parse_args()
+path = args.path
+document = open(path)
+text = document.read()
+#stopwords = set(nltk.corpus.stopwords.words('english'))
+#need to delete stopwords from text later
+words = []
+words = words + text.split()
+#print(words)
+#dict_of_words = { i: words[i] for i in range(0, len(words)) }
+#print(dict_of_words)
+most_common_words = collections.Counter(words).most_common(100)
+print(most_common_words)
 if __name__ == '__main__':
     pass
